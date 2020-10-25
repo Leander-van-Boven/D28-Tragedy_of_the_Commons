f3f05eb9467e459ae300d2220a7b895c2011ed41
JesperK123456 <47450353+JesperK123456@users.noreply.github.com>
2020-10-25 16:09:34 +0100

    changed rData

--- data_analysis/exp_1/big.R
+++ data_analysis/exp_1/big.R
@@ -13,22 +13,17 @@ merge_csv <- function(dir){
 # Parameters:
 dir <- '../../big_test'
 max_epoch <- 500
+no_params <- 9
 
-# Get full data frame
-d <- merge_csv(dir)
+analyze_exp <- function(dir, max_epoch, no_params){
+  merge_csv(dir) %>% 
+    mutate(no.agents=A+B+C+D+E) %>% 
+    group_by(Exp.Num) %>% 
+    filter(max(Epoch)==max_epoch-1) %>% 
+    ungroup %>% 
+    group_by(across(0:no_params+1)) %>% 
+    summarise(across(Resource:no.agents, list(mean=mean, sd=sd))) %>% 
+    mutate(score=no.agents_mean/no.agents_sd) %>% 
+    arrange(desc(score))
+}
 
-# Add agent count per epoch
-d$no.agents <- d %>% 
-  select(A:E) %>% 
-  rowSums
-
-no.params <- grep("Epoch", colnames(d))-1
-
-meta <- d %>%
-  group_by(Exp.Num) %>% 
-  filter(max(Epoch)==max_epoch-1) %>% 
-  ungroup %>% 
-  group_by(across(0:no.params)) %>% 
-  summarise(across(Resource:no.agents, list(mean=mean, sd=sd))) %>% 
-  mutate(score=no.agents_mean/no.agents_sd) %>% 
-  arrange(desc(score))
