9ad7f81e2b1099955ec9a8679c715130ee0859fe
JesperK123456 <47450353+JesperK123456@users.noreply.github.com>
2020-10-25 14:31:13 +0100

    added experiment analysis script

--- data_analysis/exp_1/script.R
+++ data_analysis/exp_1/script.R
@@ -2,7 +2,7 @@ library(ggplot2)
 library(dplyr)
 library(reshape2)
 
-df <- read.csv("F:\\My Drive\\year1\\1-dmas\\project-repo\\Tragedy_of_the_Commons\\out1.csv")
+df <- read.csv("..\\..\\out1.csv")
 
 df %>% 
   melt(id.vars=c('Exp.Num', 'Epoch'), measure.vars=c('A', 'B', 'C', 'D', 'E')) %>% 
